97afd99914083412d95fe18e819d3bce08c9cde0
Carlos <2092019+CarlosEsco@users.noreply.github.com>
2020-07-06 12:57:59 -0400

    Update Page.kt

--- library/src/main/java/eu/kanade/tachiyomi/source/model/Page.kt
+++ library/src/main/java/eu/kanade/tachiyomi/source/model/Page.kt
@@ -1,10 +1,12 @@
 package eu.kanade.tachiyomi.source.model
 
 import android.net.Uri
+import eu.kanade.tachiyomi.network.ProgressListener
+
 
 class Page(
         val index: Int,
         val url: String = "",
         var imageUrl: String? = null,
         var uri: Uri? = null
-)
+) : ProgressListener {
